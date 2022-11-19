d34830ce0c0939caef17df99dfa8787ba9df9994
Khulafat <114230545+Khulafat@users.noreply.github.com>
2022-11-19 19:14:53 +0700

    Update Facebook.java

--- Social_Media/Facebook.java
+++ Social_Media/Facebook.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Social_Media;
 
 /**
@@ -10,7 +6,6 @@ package Social_Media;
  */
 public class Facebook extends SocialMedia{
     
-    @Override
     public void chatting(){
         System.out.println("Facebook bisa digunakan untuk mengirim pesan");
     }
